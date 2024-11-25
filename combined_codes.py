87649b6d19f0fe2397caadf3b4373ad87d5468e4
kaat22 <t1kaat00@students.oamk.fi>
2024-11-25 13:55:41 +0200

    work on final functionality logic

--- combined_codes.py
+++ combined_codes.py
@@ -102,6 +102,52 @@ def linefollowercontroller():
 
                 gpg.right()
     #await asyncio.sleep(0.1)
+    
+def crossingdetection(linevalues,route,routeindex):
+    if (linevalues[0] < threshold and linevalues[1] < threshold and linevalues[2] < threshold and linevalues[3] < threshold and linevalues[4] < threshold
+    or linevalues[0] < threshold and linevalues[1] < threshold and linevalues[2]< threshold
+    or linevalues[2] < threshold and linevalues[3] < threshold and linevalues[4] < threshold):
+
+        counter += 1
+        global followline
+        followline = False
+        gpg.open_eyes()
+
+    while counter > 0:
+                
+        #gpg.drive_cm(7)
+        print(route[routeindex])
+        print(routeindex)
+        
+        if route[routeindex] == "r":
+            gpg.drive_cm(7)
+            gpg.orbit(90)
+            #gpg.right()
+            routeindex += 1
+                #continue
+        elif route[routeindex] == "f":
+            gpg.forward()
+            time.sleep(0.2)
+            routeindex += 1
+            #continue
+        elif route[routeindex] == "l":
+            gpg.drive_cm(7)
+            gpg.orbit(-90)
+            #gpg.left()
+            routeindex += 1
+                #continue
+        else:
+            gpg.stop()
+            gpg.close_eyes()
+            routeindex = 0
+            #exit()
+        counter = 0
+        followline = True
+        gpg.close_eyes()
+        return routeindex
+    #await asyncio.sleep(0)
+    
+        
 
 def request_route():
     try:
@@ -152,16 +198,52 @@ async def main():
     t2.start()
     counter = 0
     #route = ["r","l","l","l","f","l","l","r","r","r","r","f","r","r","f","r","f","r","s"]
-
+    attemptnum = 0
     route = []
     global route_received
-
+    global followline
+    grdclr = False
     routeindex = 0
     gpg.close_eyes()
     while True:
-
-        while not route_received:
-            
+        
+        linevalues = linefollower_easy.read()
+        if not route_received:
+            if (linevalues[0] < threshold and linevalues[1] < threshold and linevalues[2] < threshold and linevalues[3] < threshold and linevalues[4] < threshold):
+                gpg.stop()
+                followline = False
+                package_detection()
+                if package_picked_up:
+                    print("Requesting route...")
+                    time.sleep(5)
+                    followline = True
+                    route = request_route()
+                    attemptnum += 1
+                elif attemptnum > 1:
+                    followline = False
+                    gpg.stop()
+                    while len(route) == 0:
+                        print("No route received, requesting again...")
+                        route = request_route()
+                    print(route)
+                    route_received = True
+                else:
+                    print("waiting for package...")
+                    continue
+                    
+        if grdclr:
+            if (linevalues[0] < threshold and linevalues[1] < threshold and linevalues[2] < threshold and linevalues[3] < threshold and linevalues[4] < threshold):
+                gpg.stop()
+                followline = False
+                package_detection()
+                if package_picked_up:
+                    print ("waiting for package removal...")
+                    continue
+                else:
+                    followline = True
+                
+                
+            '''
             package_detection()
             global package_picked_up
             if package_picked_up:
@@ -177,54 +259,20 @@ async def main():
                 gpg.stop()
                 continue
             send_message_to_server("Route received")
+            '''
+        if route_received:
+            routeindex = crossingdetection(linevalues,route,routeindex)
+            if routeindex == 0:
+                grdclr = True
         
-        linevalues = linefollower_easy.read()
+        
         lineposition = linefollower_easy.read_position()
         q.put(lineposition)
         #t1.run()
         #t2.run()
         #print(linevalues)
         #print(lineposition)
-        if (linevalues[0] < threshold and linevalues[1] < threshold and linevalues[2] < threshold and linevalues[3] < threshold and linevalues[4] < threshold
-        or linevalues[0] < threshold and linevalues[1] < threshold and linevalues[2]< threshold
-        or linevalues[2] < threshold and linevalues[3] < threshold and linevalues[4] < threshold):
-
-            counter += 1
-            global followline
-            followline = False
-            gpg.open_eyes()
-
-        while counter > 0:
-                    
-            #gpg.drive_cm(7)
-            print(route[routeindex])
-            print(routeindex)
-            
-            if route[routeindex] == "r":
-                gpg.drive_cm(7)
-                gpg.orbit(90)
-                #gpg.right()
-                routeindex += 1
-                    #continue
-            elif route[routeindex] == "f":
-                gpg.forward()
-                time.sleep(0.2)
-                routeindex += 1
-                #continue
-            elif route[routeindex] == "l":
-                gpg.drive_cm(7)
-                gpg.orbit(-90)
-                #gpg.left()
-                routeindex += 1
-                    #continue
-            else:
-                gpg.stop()
-                gpg.close_eyes()
-                exit()
-            counter = 0
-            followline = True
-            gpg.close_eyes()
-        #await asyncio.sleep(0)
+        
 
 
 asyncio.run(main())
